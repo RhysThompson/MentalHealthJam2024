be0f32b54c16f864f4990a4f8f320c0c16ab5ea4
AlConolly <conollyallen2@gmail.com>
2024-08-18 13:04:05 -0700

    Readded footstep sounds

---
+++ Assets/_Scripts/FootStepPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using FMODUnity;
+/// <summary>
+/// FMOD sounds can't be played from ThirdPersonCharacter.cs, so this script is used to play footstep sounds.
+/// </summary>
+public class FootStepPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private string footstepEventPath;
+
+    private FMOD.Studio.EventInstance footstepEvent;
+
+    public void OnStep()
+    {
+        footstepEvent = RuntimeManager.CreateInstance(footstepEventPath);
+        footstepEvent.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+        footstepEvent.start();
+        print("Step");
+    }
+}
